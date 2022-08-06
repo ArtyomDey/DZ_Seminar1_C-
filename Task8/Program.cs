d9503220f9b1cbefd98a90e580b0aedee5ed2b6e
ArtyomDey <a.dey@mail.ru>
2022-08-06 18:27:05 +0300

    Write code task 8

---
+++ Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите целое число: ");
+int N = int.Parse(Console.ReadLine());
+int A = 1;
+while (A <= N)
+{
+    int div = A % 2;
+    if (div == 0)
+    {
+        Console.WriteLine(A);
+    }
+    A += 1;
+}
